5bfa748c453db2b8c6645eca30d4bac21aef5dcd
Grossman97 <57723613+Grossman97@users.noreply.github.com>
2020-03-02 18:09:48 +0100

    Update server.php

--- server.php
+++ server.php
@@ -8,15 +8,12 @@ $db_name = 'crud';
 $conn = mysqli_init();
 mysqli_real_connect($conn, $servername, $username, $password, $db_name, 3306);
 
-	session_start();
-	$db = mysqli_connect($servername, $username, $password, $db_name);
-	$id = 0;
 	$link = "";
 
 	if (isset($_POST['save'])) {
 		$link = $_POST['link'];
 
-		mysqli_query($db, "INSERT INTO info (link) VALUES ('$link')"); 
+		mysqli_query($conn, "INSERT INTO info (link) VALUES ('$link')"); 
 		$_SESSION['message'] = "Saved";
 		header('location: index.php');
 	}
