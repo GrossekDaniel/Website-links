b38caa78722f0bec56fd2633fcbad34dd961c15b
Grossman97 <57723613+Grossman97@users.noreply.github.com>
2020-03-03 10:43:09 +0100

    Update server.php

--- server.php
+++ server.php
@@ -1,36 +1,18 @@
 <?php
 
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
 
-foreach ($_SERVER as $key => $value) {
-  if (strpos($key, "MYSQLCONNSTR_") !== 0) {
-    continue;
-  }
-
-  $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-  $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-  $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-  $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
-define('db_name', $connectstr_dbname);
-define('username', $connectstr_dbusername);
-define('password', $connectstr_dbpassword);
-define('servername', $connectstr_dbhost);
 
 // Create connection
+$con=mysqli_init(); 
+mysqli_real_connect($con, "database4website.mysql.database.azure.com", "grossman@database4website", "zaq1@WSX", "crud", 3306);
 
-	$db = mysqli_connect($servername, $username, $password, $db_name);
 	$id = 0;
 	$link = "";
 
 	if (isset($_POST['save'])) {
 		$link = $_POST['link'];
 
-		mysqli_query($db, "INSERT INTO info (link) VALUES ('$link')"); 
+		mysqli_query($con, "INSERT INTO info (link) VALUES ('$link')"); 
 		$_SESSION['message'] = "Saved";
 		header('location: index.php');
 	}
@@ -38,7 +20,7 @@ define('servername', $connectstr_dbhost);
 	if (isset($_GET['del'])) {
         $id = $_GET['del'];
         
-		mysqli_query($db, "DELETE FROM info WHERE id=$id");
+		mysqli_query($con, "DELETE FROM info WHERE id=$id");
 		$_SESSION['message'] = "Deleted!"; 
 		header('location: index.php');
     }
