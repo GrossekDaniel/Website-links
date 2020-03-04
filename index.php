2167c5b6627540a96cda3a0c5964d039dc81c1a7
Grossman97 <57723613+Grossman97@users.noreply.github.com>
2020-03-04 17:55:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -43,8 +43,12 @@
                 </table>
                 
                 <?php if (isset($_SESSION['message'])): ?>
-                    echo $_SESSION['message']; 
-                    unset($_SESSION['message']);
+                    <div class="msg">
+                        <?php 
+                            echo $_SESSION['message']; 
+                            unset($_SESSION['message']);
+                        ?>
+                    </div>
                 <?php endif ?>
                 
             </div>
