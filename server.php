54c20dc08798088de7c4e7073cb0536a3ad74f87
Grossman97 <57723613+Grossman97@users.noreply.github.com>
2020-03-04 17:01:51 +0100

    Update server.php

--- server.php
+++ server.php
@@ -10,18 +10,14 @@ mysqli_real_connect($conn, $host, $db_user, $db_password, $db_name, 3306);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-
-public function sprawdzURL($input) {
-        return preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $input);
-}
-
-
 	$id = 0;
 	$link = "";
 
-	if (isset($_POST['save']) and (sprawdzURL($_GET['link'])!=false)) {
+	if (isset($_POST['save'])) {
 		$link = $_POST['link'];
-
+			if (!eregi('^(http://)w*.?w.w+.*$', $strona_www)){
+   die('Wpisz poprawny adres w postaci http://www.twojastrona.pl');
+   }
 		mysqli_query($conn, "INSERT INTO info (link) VALUES ('$link')"); 
 		$_SESSION['message'] = "Saved";
 		header('location: index.php');
