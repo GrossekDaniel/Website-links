fdba3689a525d64489a5aaef99531d5e2b746328
Grossman97 <57723613+Grossman97@users.noreply.github.com>
2020-03-04 17:15:35 +0100

    Update server.php

--- server.php
+++ server.php
@@ -10,14 +10,23 @@ mysqli_real_connect($conn, $host, $db_user, $db_password, $db_name, 3306);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+	public function sprawdzURL($input) {
+        	return preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $input);
+	}
 	$id = 0;
 	$link = "";
 
 	if (isset($_POST['save'])) {
 		$link = $_POST['link'];
-		mysqli_query($conn, "INSERT INTO info (link) VALUES ('$link')"); 
-		$_SESSION['message'] = "Saved";
-		header('location: index.php');
+		if sprawdzURL($link)!=true{
+			$_SESSION['message'] = "This is not a website";
+			header('location: index.php');
+		}
+		else {
+			mysqli_query($conn, "INSERT INTO info (link) VALUES ('$link')"); 
+			$_SESSION['message'] = "Saved";
+			header('location: index.php');
+		}
 	}
 
 	if (isset($_GET['del'])) {
