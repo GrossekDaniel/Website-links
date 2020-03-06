f1239af2a7444e48a02bd4d482d0652fc04e8d1b
Daniel Grossek <57723613+Grossman97@users.noreply.github.com>
2020-03-06 10:32:25 +0100

    Update server.php

--- server.php
+++ server.php
@@ -29,6 +29,47 @@ function sprawdzURL($input) {
 		}
 	}
 
+	if (isset($_POST['send'])){
+		$email = $_POST['email'];
+		$url = 'https://api.sendgrid.com/';
+ 		$user = 'grossman';
+ 		$pass = 'zaq1@WSX';
+
+ $params = array(
+      'api_user' => $user,
+      'api_key' => $pass,
+      'to' => '$email',
+      'subject' => 'testing',
+      'html' => 'testing html',
+      'text' => 'testing body',
+      'from' => 'danielgrossek@gmail.com',
+   );
+
+ $request = $url.'api/mail.send.json';
+
+ // Generate curl request
+ $session = curl_init($request);
+
+ // Tell curl to use HTTP POST
+ curl_setopt ($session, CURLOPT_POST, true);
+
+ // Tell curl that this is the body of the POST
+ curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+ // Tell curl not to return headers, but do return the response
+ curl_setopt($session, CURLOPT_HEADER, false);
+ curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+ // obtain response
+ $response = curl_exec($session);
+ curl_close($session);
+
+ // print everything out
+ print_r($response);
+	}
+
+	
+
 	if (isset($_GET['del'])) {
         $id = $_GET['del'];
         
